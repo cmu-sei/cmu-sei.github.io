7188013260a8ffaf7b7cda513b882322f3367ea9
Damon G. Morda <dgm@sei.cmu.edu>
2017-09-27 14:41:57 -0400

    Update Google Analytics property id

--- js/ga.js
+++ js/ga.js
@@ -3,5 +3,5 @@
 ,i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-145208-7', 'auto');
+ga('create', 'UA-145208-1', 'auto');
 ga('send', 'pageview');
