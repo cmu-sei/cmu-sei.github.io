1f3ee8282413cb0190f9d0b5072c8be714f4a3de
Jason Shimkoski <jdshimkoski@sei.cmu.edu>
2023-02-17 11:06:37 -0500

    update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
+  app: {
+    baseURL: '/cmu-sei.github.io/'
+  },
   modules: ['@nuxtjs/tailwindcss', 'nuxt-vitest']
 })
