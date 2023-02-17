bd3664d8c95db80d5aa96d01fcd3fe873cc77d79
Jason Shimkoski <jdshimkoski@sei.cmu.edu>
2023-02-17 11:09:28 -0500

    update nuxt config, add nojekyll file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    baseURL: '/cmu-sei.github.io/'
-  },
   modules: ['@nuxtjs/tailwindcss', 'nuxt-vitest']
 })
